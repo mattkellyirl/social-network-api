1577543b8e86eec9cbb4e77e209e6dd537d0ec14
mattkellyirl <matthew.kelly@digicom.net.au>
2024-04-28 11:51:59 +0930

    updated reactionController.js

---
+++ controllers/reactionController.js
@@ -0,0 +1,52 @@
+const Reaction = require("../models/Reaction");
+const Thought = require("../models/Thought");
+
+module.exports = {
+  // Create New Reaction
+  async newReaction(req, res) {
+    try {
+      const thoughtID = await Thought.findById(req.params.thoughtID);
+      if (!thoughtID) {
+        return res.status(404).json({ message: "Thought ID Not Found" });
+      } else {
+        await Thought.updateOne(
+          { _id: thoughtID },
+          { $addToSet: { reactions: req.body } },
+          { runValidators: true, new: true }
+        );
+        return res.status(200).json({
+          message: "Request Successful - New Reaction",
+          thoughtID,
+        });
+      }
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: "Request Failed - New Reaction", details: err });
+    }
+  },
+
+  // Delete Reaction By ID
+  async deleteReactionByID(req, res) {
+    try {
+      const thoughtID = await Thought.findById(req.params.thoughtID);
+      if (!thoughtID) {
+        return res.status(404).json({ message: "Thought ID Not Found" });
+      } else {
+        await Thought.updateOne(
+          { _id: thoughtID },
+          { $pull: { reactions: { _id: req.params.reactionID } } }
+        );
+        return res.status(200).json({
+          message: "Request Successful - Delete Reaction By ID",
+          thoughtID,
+        });
+      }
+    } catch (err) {
+      return res.status(500).json({
+        error: "Request Failed - Delete Reaction By ID",
+        details: err,
+      });
+    }
+  },
+};
