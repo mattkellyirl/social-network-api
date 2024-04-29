b91218059e690c20d2f25d038f4061d9460a760d
mattkellyirl <matthew.kelly@digicom.net.au>
2024-04-29 22:24:35 +0930

    removed unused code

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,8 @@ const router = require("express").Router();
 
 const userFriendRoutes = require("./userFriendRoutes");
 const thoughtReactionRoutes = require("./thoughtReactionRoutes");
-// const reactionRoutes = require("./reactionRoutes");
 
 router.use("/users", userFriendRoutes);
 router.use("/thoughts", thoughtReactionRoutes);
-// router.use("/reactions", reactionRoutes);
 
 module.exports = router;
